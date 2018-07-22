c0abb9ccb596fedc1f26051359492c2950aeb108
Heidi Hlavinka <hchlav@gmail.com>
2018-07-22 20:07:56 +0000

    automatically backed up by learn

--- notes.rb
+++ notes.rb
@@ -1,7 +1,11 @@
 CLI walkthrough -
 
-Welcome to Friends for Life Foster page!
-Do you want to learn about one of our cats or dogs available for foster? Enter 'cats' or 'dogs'.
+puts "Welcome to Friends for Life Foster page!"
+puts ""
+puts "Do you want to learn about one of our cats or dogs available for foster? Enter 'cats' or 'dogs'."
+
+user_input = gets.strip
+user_input = "cats" || "dogs"
 
 *If "cats" -*
 Here are our urgent needs for Dogs: *use index?
@@ -17,7 +21,13 @@ Here are our urgent needs for Cats:
 
 Which cat or dog would you like to know more about? Enter a number 1 - 10.
 *User enters 1 - 10*
-
 *Displays pet name and description*
+
 Would you like to see another cat or dog? Enter Y or N.
-*If yes, go back to "Which cat or dog...*" and waits for input*
+*If yes
+   go back to "Which cat or dog..."*
+   
+*If no 
+ *"Thank you for considering one of our foster pets! If you are ready to apply, please contact us!"*
+ *exits the program*
+ 
