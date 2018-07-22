7453f9f474012be67994e03096c0ad4cecfb65d7
Heidi Hlavinka <hchlav@gmail.com>
2018-07-22 19:55:20 +0000

    automatically backed up by learn

---
+++ notes.rb
@@ -0,0 +1,20 @@
+CLI walkthrough
+
+Welcome to Friends for Life Foster page!
+Do you want to learn about one of our cats or dogs available for foster? Enter 'cats' or 'dogs'.
+
+*If "cats" -* 
+Here are our urgent needs for Dogs: *use index?
+  -Dog name
+  -Dog name
+  -etc
+  
+*If "dogs" -*
+Here are our urgent needs for Cats:
+  -Cat name 
+  -Cat name
+  -etc
+
+Which cat or dog would you like to know more about? Enter a number 1 - 10.
+Number 
+*Displays pet name and description*
